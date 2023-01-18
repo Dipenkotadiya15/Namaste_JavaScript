23b2f2f47afbfe81f086b9aa521aadf8e626549b
Dipenkotadiya15 <dipen.adsum@gmail.com>
2023-01-18 12:08:18 +0530

    initial commit

---
+++ js/07_Undefined_Not-defined.js
@@ -0,0 +1,20 @@
+
+// var a;
+
+// console.log(a);
+// a = 10; 
+
+// if (a === undefined) {
+//     console.log('a is undefined');
+// } else {
+//     console.log('a is Not undefined');
+// }
+
+
+var a;
+console.log(a);
+a = 10;
+console.log(a);
+a = 'Hello World';
+console.log(a);
+
